1521a8529608bd52e490d8e3fdbabae531fa421e
Andrii Maiskyi <andrey@wearehelful.com>
2025-06-10 11:58:29 +0100

    feat: home. #2

--- src/home/routes/Home/Home.tsx
+++ src/home/routes/Home/Home.tsx
@@ -3,22 +3,13 @@ import { FC } from 'react';
 import { Content, Grid, Header, Page } from '@core/uikit';
 
 export const Home: FC = () => {
-  const title = 'My Channels';
-
   return (
     <Page>
-      <Header translucent>
-        <Header.Title>123</Header.Title>
+      <Header>
+        <Header.Title>My Channels</Header.Title>
       </Header>
       <Content>
         <Grid>
-          <Grid.Row flex="0 0 auto">
-            <Grid.Cell>
-              <Header collapse="condense">
-                <Header.Title size="large">{title}</Header.Title>
-              </Header>
-            </Grid.Cell>
-          </Grid.Row>
           <Grid.Row flex="0 0 auto">
             <Grid.Cell>
               {/* <FablesCreate onClick={handleOnCreateClick} /> */}
