4cf46fceac41c554b6fbb15e67007f776d1ebd6e
Andrii Maiskyi <andrey@wearehelful.com>
2025-06-11 18:42:37 +0100

    feat: search. #13

--- src/_core/navigation/components/Link/Link.tsx
+++ src/_core/navigation/components/Link/Link.tsx
@@ -1,12 +1,16 @@
 import { FC, PropsWithChildren } from 'react';
 import { generatePath, Link as RouterLink } from 'react-router-dom';
+import classNames from 'classnames';
 
 import { stringify } from '../../utils/queryString';
+
+import styles from './Link.module.scss';
 
 export type LinkParams = PropsWithChildren<{
   pathname: string;
   params?: object;
   search?: object;
+  unstyled?: boolean;
 }>;
 
 export const Link: FC<LinkParams> = ({
@@ -14,10 +18,14 @@ export const Link: FC<LinkParams> = ({
   params,
   children,
   search,
+  unstyled,
 }) => {
   return (
     // @ts-ignore
     <RouterLink
+      className={classNames(styles.root, {
+        [styles.unstyled]: unstyled,
+      })}
       to={{
         pathname: generatePath(pathname, params),
         search: stringify(search),
