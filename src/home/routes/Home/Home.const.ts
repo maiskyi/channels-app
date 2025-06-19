520ff65a4abff8670058e3c348b13f9df18183dd
Andrii Maiskyi <andrey@wearehelful.com>
2025-06-19 14:15:42 +0100

    feat: channels. #10

--- src/home/routes/Home/Home.const.ts
+++ src/home/routes/Home/Home.const.ts
@@ -1,5 +1,7 @@
 import { DTO } from '@network/api';
 
+export const TAKE = 20;
+
 export const INITIAL_DATA: DTO.GetChannelsResponse = {
   data: Array.from({ length: 3 }).map((_, index) => ({
     id: index,
