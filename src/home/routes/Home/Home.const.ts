9c28af6eecafee8384a30e07389a1bb1d611aac2
Andrii Maiskyi <andrey@wearehelful.com>
2025-06-19 12:19:20 +0100

    fix: build. #8

--- src/home/routes/Home/Home.const.ts
+++ src/home/routes/Home/Home.const.ts
@@ -2,7 +2,7 @@ import { DTO } from '@network/api';
 
 export const INITIAL_DATA: DTO.GetChannelsResponse = {
   data: Array.from({ length: 3 }).map((_, index) => ({
-    id: index.toString(),
+    id: index,
     photo: 'photo',
     title: 'title',
     userName: 'userName',
